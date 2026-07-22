46313e10a57235aef785ad5b3039e05d3f115181
harshalnagpure-sdn <harshalnagpure@smartdatainc.net>
2026-07-22 20:25:09 +0530

    initial backend setup

---
+++ categories/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+# urlpatterns = [
+#     path('categories/', CategoryListView.as_view(), name='category_list'),
+#     path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+#     path('categories/create/', CategoryCreateView.as_view(), name='category_create'),
+#     path('categories/<int:pk>/update/', CategoryUpdateView.as_view(), name='category_update'),
+#     path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
+# ]
